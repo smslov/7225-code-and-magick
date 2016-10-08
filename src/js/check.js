91d5fdf700acf82ab830859f9ceec43015535162
smslov <alexandersmyslov@Alexander-Smyslovs-MacBook.local>
2016-10-08 14:02:13 +0300

    Поправил отступы

--- src/js/check.js
+++ src/js/check.js
@@ -1,32 +1,32 @@
 var getMessage = function(a, b) {
-	if(typeof a == 'boolean') {
-		if(a) {
-			return('Я попал в ' + b);
-		}
-		else {
-			return('Я никуда не попал');
-		}
-	}
-	else if(typeof a == 'number') {
-		return('Я прыгнул на ' + (a * 100) + ' сантиметров');
-	}
-	else if(Array.isArray(a)) {
+  if(typeof a == 'boolean') {
+    if(a) {
+      return('Я попал в ' + b);
+    }
+    else {
+      return('Я никуда не попал');
+    }
+  }
+  else if(typeof a == 'number') {
+    return('Я прыгнул на ' + (a * 100) + ' сантиметров');
+  }
+  else if(Array.isArray(a)) {
     if(Array.isArray(b)) {
       var distancePath = 0;
       for (var i = 0; i < a.length && i < b.length; i++) {
         distancePath += a[i] * b[i];
       }
-    return('Я прошёл ' + distancePath + ' метров');
+      return('Я прошёл ' + distancePath + ' метров');
     }
-		else {
+    else {
       var numberOfSteps = 0;
-  		for (var i = 0; i < a.length; i++) {
-  			numberOfSteps += a[i];
-  		}
-  		return('Я прошёл ' + numberOfSteps + ' шагов');
+      for (var i = 0; i < a.length; i++) {
+        numberOfSteps += a[i];
+      }
+      return('Я прошёл ' + numberOfSteps + ' шагов');
     }
-	}
-	else {
-		return('Переданы некорректные данные');
-	}
+  }
+  else {
+    return('Переданы некорректные данные');
+  }
 }
