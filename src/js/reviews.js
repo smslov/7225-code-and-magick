d950658dee3aafd1aa20a07ddff38ad3ded8bf66
smslov <alexandersmyslov@Alexander-Smyslovs-MacBook.local>
2016-11-22 20:18:35 +0300

    пути модулей

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['load', 'review'], function(load, Review) {
+define(['./load', './review'], function(load, Review) {
   var container = document.querySelector('.reviews-list');
   var LOAD_REVIEWS_URL = 'http://localhost:1507/api/reviews';
   document.querySelector('.reviews-filter').classList.add('invisible');
